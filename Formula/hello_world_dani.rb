b6d2f0e80ec0923a867876ee83e758a42f5f7ac5
danielleinception <danielle@inceptionu.com>
2024-04-23 03:34:04 -0600

    update commit

--- Formula/hello_world_dani.rb
+++ Formula/hello_world_dani.rb
@@ -3,7 +3,7 @@ class HelloWorldDani < Formula
   homepage "https://github.com/danibarker/tap-tap-dani-barker"
   url "file:///Users/daniellebarker/programming/personal/homebrew/tap-tap-dani-barker/hello_world_dani.sh"
   version "0.1"
-  sha256 "133f05b63a017f8fe4ecd9cc4d1af69a8511e078eada11e83b10afb24cd54380"
+  sha256 "3c89be10a65a483d5418d2b8ca58bf2fe09ef22ae6f9db5df0e4b4f40a5b898f"
   def install
     bin.install "hello_world_dani.sh" => "hello_world_dani"
   end
