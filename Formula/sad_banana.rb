f77112ec572c3dd878ab36530f1b143cb6154e6e
danielleinception <danielle@inceptionu.com>
2024-04-23 04:26:47 -0600

    update commit

--- Formula/sad_banana.rb
+++ Formula/sad_banana.rb
@@ -3,10 +3,7 @@ class SadBanana < Formula
   version "1.0.0"
   sha256 "3c89be10a65a483d5418d2b8ca58bf2fe09ef22ae6f9db5df0e4b4f40a5b898f"
   def install
-    for a in 1..50 do
-      exec('echo', 'Hello, My Friend')
-      puts "Hello, Sexy..."
-    end
+    50.times { puts "Hello, Sexy..." }
     bin.install "sad_banana.sh" => "sad_banana"
   end
 end
