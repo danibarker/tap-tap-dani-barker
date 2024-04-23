52658b1bbf63d42c0a948118cddb1f374c94bd4a
danielleinception <danielle@inceptionu.com>
2024-04-23 04:18:36 -0600

    update commit

--- Formula/WHAT_DO_YOU_WANT_ME_TO_NAME_THIS_SCRIPT.rb
+++ Formula/WHAT_DO_YOU_WANT_ME_TO_NAME_THIS_SCRIPT.rb
@@ -1,4 +1,4 @@
-class HelloWorldDani < Formula
+class WhatDoYouWantMeToNameThisScript < Formula
   url "file:///Users/daniellebarker/programming/personal/homebrew/tap-tap-dani-barker/WHAT_DO_YOU_WANT_ME_TO_NAME_THIS_SCRIPT.sh"
   version "1.0.0"
   sha256 "3c89be10a65a483d5418d2b8ca58bf2fe09ef22ae6f9db5df0e4b4f40a5b898f"
