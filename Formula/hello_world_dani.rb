81ae1f649e16e3d54beccd336f8156adfd10c158
danielleinception <danielle@inceptionu.com>
2024-04-23 03:44:56 -0600

    update commit

--- Formula/hello_world_dani.rb
+++ Formula/hello_world_dani.rb
@@ -5,10 +5,6 @@ class HelloWorldDani < Formula
   version "0.1"
   sha256 "3c89be10a65a483d5418d2b8ca58bf2fe09ef22ae6f9db5df0e4b4f40a5b898f"
   def install
-    bin.install "hello_world_dani.sh" => "hello_world_dani"
   end
   puts "Hello, World..."
-  test do
-    assert_equal "Hello, World...", shell_output("\#{bin}/hello_world_dani").strip
-  end
 end
