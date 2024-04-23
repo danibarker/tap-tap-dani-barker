4396dfb3a4f85a2efbd95b88b075a73af20d2584
danielleinception <danielle@inceptionu.com>
2024-04-23 03:54:27 -0600

    update commit

--- Formula/hello_world_dani.rb
+++ Formula/hello_world_dani.rb
@@ -1,7 +1,9 @@
 class HelloWorldDani < Formula
   url "file:///Users/daniellebarker/programming/personal/homebrew/tap-tap-dani-barker/hello_world_dani.sh"
   version "1.0.0"
+  sha256 "3c89be10a65a483d5418d2b8ca58bf2fe09ef22ae6f9db5df0e4b4f40a5b898f"
   def install
     puts "Hello, Sexy..."
+    bin.install "hello_world_dani.sh" => "fuckme"
   end
 end
