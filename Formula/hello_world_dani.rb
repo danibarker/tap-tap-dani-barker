b8243ac7b7fbd774ffc6d5bc17d33d931db4279c
danielleinception <danielle@inceptionu.com>
2024-04-23 03:09:06 -0600

    update commit

---
+++ Formula/hello_world_dani.rb
@@ -0,0 +1,13 @@
+class hello_world_dani < Formula
+  desc "A simple 'Hello, World!' script"
+  homepage "https://github.com/danibarker/tap-tap-dani-barker"
+  url "file:///Users/daniellebarker/programming/personal/homebrew/tap-tap-dani-barker/hello_world_dani.sh"
+
+  def install
+    bin.install "hello_world_dani.sh" => "hello_world_dani"
+  end
+
+  test do
+    assert_equal "Hello, World!", shell_output("\#{bin}/hello_world_dani").strip
+  end
+end
